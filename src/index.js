35ae25d6660a01a4b4e4d1f94e13b6e8f4d5762a
talentforest <talentforest0501@gmail.com>
2022-04-01 05:39:49 +0900

    ScrollToTop

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css"
 import { BrowserRouter } from "react-router-dom"
+import ScrollToTop from 'util/ScrollToTop';
 
 ReactDOM.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
   document.getElementById("root")
